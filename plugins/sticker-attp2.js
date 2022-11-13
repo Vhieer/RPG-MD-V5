e1c7d3558d40bdd6a4066001bd42d5efdf43cda1
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 18:18:11 +0700

    Update sticker-attp2.js

--- plugins/sticker-attp2.js
+++ plugins/sticker-attp2.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
   m.reply('ᴘʀᴏꜱᴇꜱ...')
-  let res = `https://api.lolhuman.xyz/api/ttp2?apikey=64333e1c050ced435defe154&text=${response[0]}&apikey=Xynoz`
+  let res = `https://botcahx.ddns.net/api/maker/attp?text=${response[0]}`
   conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
 }
 handler.help = ['attp2 <teks>']
-handler.tags = ['sticker' , 'premium']
+handler.tags = ['sticker' , '']
 handler.command = /^(attp2)$/i
 handler.limit = true
-handler.premium = true
+handler.premium = false
 
 export default handler
