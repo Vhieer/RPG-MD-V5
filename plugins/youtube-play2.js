3522a1097e6afa5aa18463a202ef42c3fe923061
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 18:55:21 +0700

    Update youtube-play2.js

--- plugins/youtube-play2.js
+++ plugins/youtube-play2.js
@@ -1,30 +1,30 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler = async (m, {command, conn, text}) => {
-if (!text) throw `[❗INFO❗] Masukan Nama Lagu Yang Ingin Di Cari\n\n*—◉ Contoh:\n#play.1 Good Feeling - Flo Rida*`
+if (!text) throw `[❗INFO❗] Masukan Link yang  Yang Ingin Di Download \n\n*—◉ Contoh:\n#play.1 https://www.youtube.com/watch?v=cvAbJt3xQaA*`
 try {
 if (command == 'play.1') {
 conn.reply(m.chat, `*_⏳Wait,Audio Sedang Di Proses..._⏳*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'AUDIO',
-body: 'YukiBot',         
-previewType: 0, thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-sourceUrl: `https://youtu.be/pwLZpdfO8AU`}}})
-let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
+body: 'Secret MD',         
+previewType: 0, thumbnail: await (await fetch(`https://telegra.ph/file/b90042f0d70b230d7c72e.jpg`)).buffer(),
+sourceUrl: `https://www.youtube.com/watch?v=cvAbJt3xQaA`}}})
+let res = await fetch("https://api.lolhuman.xyz/api/ytaudio?apikey=SadTeams&url="+text)
 let json = await res.json()
 conn.sendFile(m.chat, json.result.url, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
 if (command == 'play.2') {
 conn.reply(m.chat, `*_⏳ Wait,Video Sedang Di Proses...⏳_*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'VIDEO',
-body: 'ᴹᴿ᭄ King Of Bear ×፝֟͜×',         
-previewType: 0, thumbnail: await (await fetch('https://telegra.ph/file/6ab4daac226292a112540.jpg')).buffer(),
-sourceUrl: `https://youtu.be/pwLZpdfO8AU`}}})
-let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text)
+body: 'By Reeya',         
+previewType: 0, thumbnail: await (await fetch('https://telegra.ph/file/b90042f0d70b230d7c72e.jpg')).buffer(),
+sourceUrl: `https://www.youtube.com/watch?v=cvAbJt3xQaA`}}})
+let res = await fetch("https://api.lolhuman.xyz/api/ytvideo?apikey=SadTeams&url="+text)
 let json = await res.json()
-conn.sendFile(m.chat, json.result.url, 'error.mp4', `_ᴹᴿ᭄ King Of Bear ×፝֟͜×_`, m)}
+conn.sendFile(m.chat, json.result.url, 'error.mp4', `Secret MD`, m)}
 }catch(e){
-m.reply('*[❗INFO❗] ERROR,TIDAK DAPAT MENCARI LAGU TERSEBUT*')
+m.reply('*[❗INFO❗] ERROR,TIDAK DAPAT MENDOWNLOAD MP3/MP4 TERSEBUT*')
 console.log(e)
 }}
 handler.help = ['play.1' , 'play.2'].map(v => v + ' <texto>')
