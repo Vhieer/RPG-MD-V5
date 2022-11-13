4000ece7a01d5222271878e563ca0f84af4b7173
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 16:37:26 +0700

    Update game-tebakumur.js

--- plugins/game-tebakumur.js
+++ plugins/game-tebakumur.js
@@ -9,7 +9,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Masukan Teksnya', m)
 
     await m.reply('Searching...')
-	axios.get(`https://api.lolhuman.xyz/api/tebakumur?apikey=2548ae0a1ad4d3233004f218&name=${text}`).then ((res) => {
+	axios.get(`https://api.lolhuman.xyz/api/tebakumur?apikey=SadTeams&name=${text}`).then ((res) => {
 	 	let hasil = `Namamu : ${text}\nUmurmu : ${res.data.result.age}`
 
     conn.reply(m.chat, hasil, m)
