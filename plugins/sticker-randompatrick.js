83671326fec83a1058ec7ac717a9a625da3b9d3d
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 18:19:57 +0700

    Update sticker-randompatrick.js

--- plugins/sticker-randompatrick.js
+++ plugins/sticker-randompatrick.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let res = 'https://api.lolhuman.xyz/api/sticker/patrick?apikey=64333e1c050ced435defe154'
+	let res = 'https://api.lolhuman.xyz/api/sticker/patrick?apikey=SadTeams'
 	conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
 }
 handler.command = /^(spatrick)$/i
