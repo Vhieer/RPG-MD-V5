91942a763858a19e60012616df3402ffbb8e9a40
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 18:09:13 +0700

    Update ryhar-ttmp3.js

--- plugins/ryhar-ttmp3.js
+++ plugins/ryhar-ttmp3.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'Url Nya Mana..?\nContoh : .ttaudio https://vt.tiktok.com/ZSRDgWawB/'
   m.reply('ᴘʀᴏꜱᴇꜱ...')
-  let res = `https://api.lolhuman.xyz/api/tiktokmusic?apikey=2548ae0a1ad4d3233004f218&url=${args[0]}`
+  let res = `https://api.lolhuman.xyz/api/tiktokmusic?apikey=SadTeams&url=${args[0]}`
   conn.sendFile(m.chat, res, 'xynz.opus', `┏┉━━━━━━━━━━━❏
 ┆ *TIKTOK MP3*
 ├┈┈┈┈┈┈┈┈┈┈┈
