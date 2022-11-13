41732e4c7e4c20ca827cb1fc6007e8dd04e805d0
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 17:59:28 +0700

    Update pornhub.js

--- plugins/pornhub.js
+++ plugins/pornhub.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
   if (!args[0]) throw 'Teks Nya Banh?\nContoh :\!pornhub tes|tes'
   m.reply('Proses...')
-  let res = `https://api.lolhuman.xyz/api/textprome2/pornhub?apikey=64333e1c050ced435defe154&text1=${response[0]}&text2=${response[1]}`
-  conn.sendFile(m.chat, res, 'pornhub.jpg', `*Done Banh*\n_©by ᴹᴿ᭄ King Of Bear ×፝֟͜×_`, m, false)
+  let res = `https://api.lolhuman.xyz/api/textprome2/pornhub?apikey=SadTeams&text1=${response[0]}&text2=${response[1]}`
+  conn.sendFile(m.chat, res, 'pornhub.jpg', `*Done Banh*\n_©by Reeya`, m, false)
 }
 handler.help = ['pornhub'].map(v => v + ' <text1>|<teks2>')
 handler.tags = ['maker']
