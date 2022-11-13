f434f8a3e091b900b0b880ffbc26a663c6ef573a
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 17:31:42 +0700

    Update nulis-googglesugest.js

--- plugins/nulis-googglesugest.js
+++ plugins/nulis-googglesugest.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Teks\nContoh : !gogglesugest teks1|teks2|teks3'
   m.reply('ᴘʀᴏꜱᴇꜱ...')
-  let res = `https://api.lolhuman.xyz/api/gsuggest?apikey=64333e1c050ced435defe154&text1=${response[0]}&text2=${response[1]}&text3=${response[2]}&apikey=Xynoz`
+  let res = `https://api.lolhuman.xyz/api/gsuggest?&text1=${response[0]}&text2=${response[1]}&text3=${response[2]}&apikey=SadTeams`
   conn.sendFile(m.chat, res, 'xynz.jpg', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
 }
 handler.help = ['googlesugest'].map(v => v + ' <text>')
