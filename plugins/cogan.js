cc266af2ec39d24e5813c907c9b1c7c69a8475ac
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-13 16:07:47 +0700

    Update cogan.js

--- plugins/cogan.js
+++ plugins/cogan.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.lolhuman.xyz/api/random/cogan?apikey=64333e1c050ced435defe154'
-	conn.sendButton(m.chat, '*–––| Sukses |–––*', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+	let url = 'https://api.lolhuman.xyz/api/random/cogan?apikey=SadTeams'
+	conn.sendButton(m.chat, '*â€“â€“â€“| Sukses |â€“â€“â€“*', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(cogan)$/i
 handler.tags = ['random']
